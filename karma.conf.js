e9000faeec4607465e6f3496a12bcc25a808b40d
Hugh Guiney <hugh@hughguiney.com>
2019-07-31 02:43:19 -0400

    Attempt to solve Travis CI build failure on macOS

--- karma.conf.js
+++ karma.conf.js
@@ -4,6 +4,8 @@ const merge = require( 'deepmerge' );
 module.exports = ( config ) => {
   config.set(
     merge( createDefaultConfig( config ), {
+      "browsers": ["ChromeHeadlessNoSandbox"],
+
       "files": [
         // runs all files ending with .test in the test folder,
         // can be overwritten by passing a --grep flag. examples:
@@ -17,6 +19,35 @@ module.exports = ( config ) => {
       "esm": {
         // if you are using 'bare module imports' you will need this option
         "nodeResolve": true,
+      },
+
+      // https://github.com/karma-runner/karma/issues/2652#issuecomment-431843769
+      "captureTimeout": 210000,
+      "browserDisconnectTolerance": 3,
+      "browserDisconnectTimeout": 210000,
+      "browserNoActivityTimeout": 210000,
+
+      "customLaunchers": {
+        "ChromeHeadlessNoSandbox": {
+          "base": "ChromeHeadless",
+          "flags": [
+            "--disable-background-timer-throttling",
+            "--disable-default-apps",
+            "--disable-dev-shm-usage",
+            "--disable-device-discovery-notifications",
+            "--disable-gpu",
+            "--disable-popup-blocking",
+            "--disable-renderer-backgrounding",
+            "--disable-setuid-sandbox",
+            "--disable-translate",
+            "--disable-web-security",
+            "--enable-logging",
+            "--no-first-run",
+            "--no-sandbox",
+            "--no-default-browser-check",
+            "--remote-debugging-port=9222",
+          ],
+        },
       },
     } ),
   );
