33222e51573553293855e24b16773394f237d474
Hugh Guiney <hugh@hughguiney.com>
2022-01-09 16:44:09 -0500

    chore: Debug Netlify

---
+++ build.sh
@@ -0,0 +1,6 @@
+./node_modules/meta/bin/meta git update \
+  && ./node_modules/meta/bin/meta git pull \
+  && bundle exec jekyll build \
+  && mv _site/homepage/* _site/ \
+  && rm -rf _site/homepage/ \
+  && ls _site/img/
